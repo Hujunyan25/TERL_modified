afac1998d5aed5a82fc0e8e9e969648f2a55cd53
hujunyan <hujunyan_1@163.com>
2026-03-24 12:39:58 +0800

    修改了baseline代码

--- run_experiments_baseline.py
+++ run_experiments_baseline.py
@@ -115,7 +115,7 @@ def evaluation(states, agent, evader_agent, eval_env: MarineEnv, use_rl=True, us
         # Record the minimum distance throughout the experiment
         experiment_min_distance = min(experiment_min_distance, min_distance)
 
-        end_episode = (length >= 1000) or len([pursuer for pursuer in eval_env.pursuers if
+        end_episode = (length >= 3000) or len([pursuer for pursuer in eval_env.pursuers if
                                                not pursuer.deactivated]) < 3 or eval_env.check_all_evader_is_captured()
         if end_episode:
             logger.info(
@@ -151,7 +151,6 @@ def exp_setup(envs, eval_schedule, i):
     for test_env in envs:
         test_env.num_pursuers = eval_schedule["num_pursuers"][i]
         test_env.num_evaders = eval_schedule["num_evaders"][i]
-        test_env.num_cores = eval_schedule["num_cores"][i]
         test_env.num_obs = eval_schedule["num_obstacles"][i]
         test_env.min_pursuer_evader_init_dis = eval_schedule["min_pursuer_evader_init_dis"][i]
 
@@ -177,11 +176,11 @@ def dashboard(eval_schedule, indent=0):
 
 def run_experiment(eval_schedules, index):
     """Run the experiment with the specified evaluation schedules."""
-    agents = [Terl_agent, IQN_agent, Mean_agent, Dqn_agent]
-    evader_agents = [evader_agent1, evader_agent2, evader_agent3, evader_agent4]
+    agents = [Terl_agent]
+    evader_agents = [evader_agent1]
     names = model_name
-    envs = [test_env_1, test_env_2, test_env_3, test_env_4]
-    evaluations = [evaluation, evaluation, evaluation, evaluation]
+    envs = [test_env_1]
+    evaluations = [evaluation]
 
     color_palette = ["#2E7BA6", "#B46FA2", "#2A8C66", "#C78A2A", "#4B61C6", "#E0BFE0", "#3D5A80", "#914E25"]
 
@@ -460,7 +459,7 @@ def initialize_wandb(config):
         wandb.init(
             project=args.project,
             group=args.group,
-            name=f"Ablation Experiment",
+            name=f"Baseline Experiment",
             config=config,
         )
         return True
@@ -522,10 +521,7 @@ if __name__ == "__main__":
 
     # Model names
     model_name = [
-        "TERL",
         "IQN",
-        "MEAN",
-        "DQN",
     ]
 
     save_dir = f"TrainedModels/{model_name[0]}"
@@ -547,29 +543,7 @@ if __name__ == "__main__":
     Terl_agent.load_model(model_dir, device)
     evader_agent1 = ApfAgent(test_env_1.evaders[0].a, test_env_1.evaders[0].w)
 
-    save_dir = f"TrainedModels/{model_name[1]}"
-    model_dir = os.path.join(project_root, save_dir)
 
-    test_env_2 = MarineEnv(seed)
-    IQN_agent = Agent(device=device, model_name=model_name[1])
-    IQN_agent.load_model(model_dir, device)
-    evader_agent2 = ApfAgent(test_env_2.evaders[0].a, test_env_2.evaders[0].w)
-
-    save_dir = f"TrainedModels/{model_name[2]}"
-    model_dir = os.path.join(project_root, save_dir)
-
-    test_env_3 = MarineEnv(seed)
-    Mean_agent = Agent(device=device, model_name=model_name[2])
-    Mean_agent.load_model(model_dir, device)
-    evader_agent3 = ApfAgent(test_env_3.evaders[0].a, test_env_3.evaders[0].w)
-
-    save_dir = f"TrainedModels/{model_name[3]}"
-    model_dir = os.path.join(project_root, save_dir)
-
-    test_env_4 = MarineEnv(seed)
-    Dqn_agent = Agent(device=device, model_name=model_name[3], use_iqn=False)
-    Dqn_agent.load_model(model_dir, device)
-    evader_agent4 = ApfAgent(test_env_4.evaders[0].a, test_env_4.evaders[0].w)
 
     run_experiment(exp_schedule, args.config)
     wandb.finish()
